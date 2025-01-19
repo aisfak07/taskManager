c41d796dc2fcf8d7d1c9d021e438e9bcd4e76aa5
aisfak07 <isfakahmed163@gmail.com>
2025-01-19 08:46:32 +0300

    Allowed host added

--- tmserver/tmserver/settings.py
+++ tmserver/tmserver/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['taskmanager-mv8m.onrender.com']
+ALLOWED_HOSTS = ['taskmanager-mv8m.onrender.com', 'http://localhost:5173/']
 
 
 # Application definition
